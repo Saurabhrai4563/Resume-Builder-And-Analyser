3a725dc42f72beefcdfbab3ba8849b0b2c632532
Saurabhrai4563 <122366063+Saurabhrai4563@users.noreply.github.com>
2026-06-15 02:37:08 +0530

    add style for Ui and created api for communicate

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,7 +1,47 @@
 import React from "react";
-
+import { Link } from "react-router";
+import "../auth.form.scss";
+import "../../../style.scss";
 const Login = () => {
-  return <div>Login</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+  };
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              name="email"
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              name="password"
+              required
+            />
+          </div>
+          <button className="button primary-button" type="submit">
+            Login
+          </button>
+        </form>
+        <p>
+          Don't have an account? <Link to={"/register"}>Register</Link>
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,7 +1,56 @@
 import React from "react";
-
+import { Link, useNavigate } from "react-router";
 const Register = () => {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+  };
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              placeholder="Enter your username"
+              name="username"
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              name="email"
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              name="password"
+              required
+            />
+          </div>
+          <button className="button primary-button" type="submit">
+            Register
+          </button>
+        </form>
+        <p>
+          Already have an account? <Link to={"/login"}>Login</Link>
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default Register;
