4f449f9952b8f9a84748137ec264ec4929a2a495
Saurabhrai4563 <122366063+Saurabhrai4563@users.noreply.github.com>
2026-06-15 02:48:57 +0530

    created state layer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { RouterProvider } from "react-router";
 import { router } from "./app.routes.jsx";
+import { AuthProvider } from "./features/auth/auth.contex.jsx";
 function App() {
   return (
     <div>
+      <AuthProvider></AuthProvider>
       <RouterProvider router={router} />
     </div>
   );
